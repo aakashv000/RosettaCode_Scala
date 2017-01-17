eb8a49d0bd4efc38cf1cb4c9afd5f1fd6d00aa86
aakashv000 <aakashv000@gmail.com>
2017-01-18 00:25:55 +0530

    Add files via upload

---
+++ babbage.scala
@@ -0,0 +1,18 @@
+//Babbage Problem
+
+object babbage{
+	def main( args:Array[String] ){
+		
+		var x:Int = 524		//Sqrt of 269696 = 519.something
+		
+		while( (x*x) % 1000000 != 269696 ){
+			
+			if( x%10 == 4 )
+				x = x+2
+			else
+				x = x+8
+		}
+		
+		println("The smallest positive integer whose square ends in 269696 = " + x )
+	}
+}
